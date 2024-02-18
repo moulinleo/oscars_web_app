9c891114ce79a117114966d7f04ca8a7b8092682
moulinleo <moulinleo17@gmail.com>
2024-02-18 18:11:22 +0100

    names

--- app.py
+++ app.py
@@ -5,10 +5,14 @@ import pandas as pd
 from warnings import filterwarnings 
 filterwarnings("ignore")
 
+
 app = Flask(__name__, static_url_path='/static')
 
+# Dictionary to store user votes
+user_votes = {}
 
-def scrap_nominees(url="https://en.wikipedia.org/wiki/96th_Academy_Awards"):
+
+def scrap_nominees(url="https://en.wikipedia.org/wiki/96th_Academy_Awards", keep='all'):
     
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -40,9 +44,15 @@ def scrap_nominees(url="https://en.wikipedia.org/wiki/96th_Academy_Awards"):
             df_nominees = df_nominees.append({'category': category, 'nominees': nominees}, ignore_index=True)
 
     nominees_dict = dict(zip(df_nominees['category'], df_nominees['nominees']))
+    
+    
+    # Keep only the first two keys
+    if str(keep).isdigit():
+        nominees_dict = {key: nominees_dict[key] for key in list(nominees_dict.keys())[:keep]}
+    
     return nominees_dict
 
-def calculate_points(user_vote):
+def calculate_points(user_vote):    
     total_points = 0
     for category, vote in user_vote.items():
         if actual_winners[category] == vote:
@@ -57,43 +67,38 @@ def calculate_points(user_vote):
             else:
                 total_points += 1
     return total_points
+
 app.jinja_env.globals.update(calculate_points=calculate_points)
 
 
 
-
-categories = scrap_nominees(url="https://en.wikipedia.org/wiki/96th_Academy_Awards")
-
-
-# Keep only the first two keys
-num_keys_to_keep = 2
-categories = {key: categories[key] for key in list(categories.keys())[:num_keys_to_keep]}
+categories = scrap_nominees(url="https://en.wikipedia.org/wiki/96th_Academy_Awards", keep=2)
 
 
 actual_winners = {category: None for category in categories}
 
-votes = []
 
 @app.route('/')
 def index():
     return render_template('index.html', categories=categories)
 
+
 @app.route('/vote', methods=['POST'])
 def vote():
-    global votes
+    user_name = request.form.get('user_name')
+    if not user_name:
+        return redirect(url_for('index'))
+    
     user_vote = {category: request.form[category] for category in categories}
-    votes.append(user_vote)
+    user_votes[user_name] = user_vote
     return redirect(url_for('results'))
+
 
 @app.route('/results')
 def results():
-    return render_template('results.html', votes=votes, actual_winners=actual_winners)
+    return render_template('results.html', user_votes=user_votes, actual_winners=actual_winners)
 
-@app.route('/update_results', methods=['POST'])
-def update_results():
-    global actual_winners
-    actual_winners = {category: request.form[category] for category in categories}
-    return redirect(url_for('results'))
+
 
 @app.route('/enter_winners', methods=['GET', 'POST'])
 def enter_winners():
