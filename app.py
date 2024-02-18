c2085cc74dbdb733580de91184759b699641fc2d
moulinleo <moulinleo17@gmail.com>
2024-02-18 16:55:21 +0100

    scrapped from wiki

--- app.py
+++ app.py
@@ -1,16 +1,50 @@
 from flask import Flask, render_template, request,  redirect, url_for
-
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+from warnings import filterwarnings 
+filterwarnings("ignore")
 
 app = Flask(__name__, static_url_path='/static')
 
-# Sample data - Replace with your actual nominees
-categories = {
-    'Best Picture': ['Nominee 1', 'Nominee 2', 'Nominee 3'],
-    'Best Actor': ['Actor 1', 'Actor 2', 'Actor 3'],
-    
-    
-}
 
+def scrap_nominees(url="https://en.wikipedia.org/wiki/96th_Academy_Awards"):
+    
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    indiatable=soup.find('table',{'class':"wikitable"}, role="presentation")
+    
+    # Create an empty DataFrame to store the nominees
+    df_nominees = pd.DataFrame(columns=['category', 'nominees'])
+
+    # Extract rows from the table
+    rows = indiatable.find_all('tr')
+
+    # Iterate through rows and print the content
+    for row in rows:
+        # Extract cells from the row
+        cells = row.find_all(['th', 'td'])
+        
+        # Extract text from each cell
+        cell_data = [cell.get_text(strip=False) for cell in cells]
+        
+        for col in [0,1]:
+            try:
+                split_cell = cell_data[col].split('\n')[:-1]
+            except:
+                continue
+            category = split_cell[0]
+            nominees = split_cell[1:]
+            
+            df_nominees = df_nominees.append({'category': category, 'nominees': nominees}, ignore_index=True)
+
+    nominees_dict = dict(zip(df_nominees['category'], df_nominees['nominees']))
+    return nominees_dict
+
+
+
+categories = scrap_nominees(url="https://en.wikipedia.org/wiki/96th_Academy_Awards")
 
 actual_winners = {category: None for category in categories}
 
